39698872e9d1a01b1f11bb5c39971d643bb72241
NishanthRishuJha <nishanthrishujha@gmail.com>
2026-01-19 15:52:57 +0530

    Update navbar active link color and font

--- frontend/skillbridge/src/components/Navbar.jsx
+++ frontend/skillbridge/src/components/Navbar.jsx
@@ -1,15 +1,48 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// import "./Navbar.css";
+// export default function Navbar() {
+//   return (
+//     <nav style={{padding:"10px", background:"#222", color:"white", display:"flex", alignItems:"center"}}>
+//       <Link to="/" style={{marginRight:10}}>Home</Link>
+//       <Link to="/login" style={{marginRight:10}}>Login</Link>
+//       <Link to="/register"style={{marginRight:10}}>Register</Link>
+//       <Link to="/internships" style={{marginRight:10}}>Internships</Link>
+//       <Link to="/post-internship" style={{marginRight:10}}>Post Internship</Link>
+//       <Link to="/profile">Profile</Link>
+
+//     </nav>
+//   );
+// }
+
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+
 export default function Navbar() {
   return (
-    <nav style={{padding:"10px", background:"#222", color:"white"}}>
-      <Link to="/" style={{marginRight:10}}>Home</Link>
-      <Link to="/login" style={{marginRight:10}}>Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/internships" style={{marginRight:10}}>Internships</Link>
-      <Link to="/post-internship" style={{marginRight:10}}>Post Internship</Link>
-      <Link to="/profile">Profile</Link>
+    <nav className="navbar">
+      <NavLink to="/" className="nav-link">
+        Home
+      </NavLink>
 
+      <NavLink to="/login" className="nav-link">
+        Login
+      </NavLink>
+
+      <NavLink to="/register" className="nav-link">
+        Register
+      </NavLink>
+
+      <NavLink to="/internships" className="nav-link">
+        Internships
+      </NavLink>
+
+      <NavLink to="/post-internship" className="nav-link">
+        Post Internship
+      </NavLink>
+
+      <NavLink to="/profile" className="nav-link">
+        Profile
+      </NavLink>
     </nav>
   );
 }
